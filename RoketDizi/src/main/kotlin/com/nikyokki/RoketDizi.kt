831237889078eb22ea99493b8e11a79ee1eee558
nikyokki <6808128+nikyokki@users.noreply.github.com>
2025-06-15 18:37:10 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- RoketDizi/src/main/kotlin/com/nikyokki/RoketDizi.kt
+++ RoketDizi/src/main/kotlin/com/nikyokki/RoketDizi.kt
@@ -37,7 +37,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class RoketDizi : MainAPI() {
-    override var mainUrl = "https://roketdizi.nl"
+    override var mainUrl = "https://www.roketdizi.live"
     override var name = "RoketDizi"
     override val hasMainPage = true
     override var lang = "tr"
