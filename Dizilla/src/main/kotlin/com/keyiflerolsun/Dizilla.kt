244f79e70f14237f44a398d377a55c0e5a7c0f2b
patr0n <eren1371@gmail.com>
2025-07-03 18:10:44 +0300

    Update Dizilla.kt

--- Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
+++ Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
@@ -1,13 +1,11 @@
-// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
-
 package com.keyiflerolsun
 
 import android.util.Base64
+import android.util.Log
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.lagradost.api.Log
 import com.lagradost.cloudstream3.Actor
 import com.lagradost.cloudstream3.Episode
 import com.lagradost.cloudstream3.ErrorLoadingException
@@ -31,13 +29,16 @@ import com.lagradost.cloudstream3.newTvSeriesSearchResponse
 import com.lagradost.cloudstream3.toRatingInt
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.syncproviders.SyncIdName
+import com.lagradost.cloudstream3.network.CloudflareKiller
+import okhttp3.Interceptor
+import okhttp3.Response
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import java.util.Calendar
-import okhttp3.Interceptor
-import okhttp3.Response
-import com.lagradost.cloudstream3.network.CloudflareKiller
-
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 
 class Dizilla : MainAPI() {
@@ -48,29 +49,30 @@ class Dizilla : MainAPI() {
     override val hasQuickSearch = true
     override val supportedTypes = setOf(TvType.TvSeries)
 
+    override var sequentialMainPage = true
+    override var sequentialMainPageDelay = 150L
+    override var sequentialMainPageScrollDelay = 150L
 
-    // ! CloudFlare bypass
-    override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
-    override var sequentialMainPageDelay       = 50L  // ? 0.05 saniye
-    override var sequentialMainPageScrollDelay = 50L  // ? 0.05 saniye
-
-    // ! CloudFlare v2
     private val cloudflareKiller by lazy { CloudflareKiller() }
-    private val interceptor      by lazy { CloudflareInterceptor(cloudflareKiller) }
+    private val interceptor by lazy { CloudflareInterceptor(cloudflareKiller) }
 
     class CloudflareInterceptor(private val cloudflareKiller: CloudflareKiller): Interceptor {
         override fun intercept(chain: Interceptor.Chain): Response {
-            val request  = chain.request()
+            val request = chain.request()
             val response = chain.proceed(request)
-            val doc      = Jsoup.parse(response.peekBody(1024 * 1024).string())
+            val doc = Jsoup.parse(response.peekBody(1024 * 1024).string())
 
-            if (doc.select("title").text() == "Just a moment..." || doc.select("title").text() == "Bir dakika lütfen...") {
+            if (doc.html().contains("verifying")) {
                 return cloudflareKiller.intercept(chain)
             }
+
             return response
         }
- 
     }
+    
+    override val supportedSyncNames = setOf(
+        SyncIdName.Simkl
+    )
 
     override val mainPage = mainPageOf(
         "${mainUrl}/tum-bolumler" to "Altyazılı Bölümler",
@@ -88,57 +90,27 @@ class Dizilla : MainAPI() {
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val yil = Calendar.getInstance().get(Calendar.YEAR)
-        val url = "$mainUrl/api/bg/findSeries?releaseYearStart=1900&releaseYearEnd=$yil&imdbPointMin=5&imdbPointMax=10&categoryIdsComma=KATID&countryIdsComma=&orderType=date_desc&languageId=-1&currentPage=$page&currentPageCount=24&queryStr=&categorySlugsComma=&countryCodesComma="
-        if (request.data.contains("dizi-turu")) {
-            if (page > 1) {
-                val document = app.get(request.data, interceptor = interceptor).document
-                document.selectFirst("div.radio-toolbar")?.select("label")?.forEachIndexed { index, element ->
-                    if (element.text() == request.name) {
-                        val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
-                        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                        val catId = document.selectFirst("div.radio-toolbar")?.select("input")!![index].attr("value")
-                        val encodedDoc = app.post(url.replace("KATID", catId))
-                        val encoded: SearchResult = objectMapper.readValue(encodedDoc.toString())
-                        val decodedJson = Base64.decode(encoded.response, Base64.DEFAULT)
-                        val decoded: PageData = objectMapper.readValue(decodedJson.decodeToString())
-                        val diziler = decoded.result?.map { it.sayfalar() }
-                        return newHomePageResponse(request.name, diziler!!)
-                    }
-                }
-                return newHomePageResponse(request.name, listOf())
-            } else {
-                val document = app.get(request.data).document
-                val diziler = document.select("span.watchlistitem-").mapNotNull { it.diziler() }
-                return newHomePageResponse(request.name, diziler)
-            }
+        var document = app.get(request.data, interceptor = interceptor).document
+        val home = if (request.data.contains("dizi-turu")) {
+            document.select("span.watchlistitem-").mapNotNull { it.diziler() }
         } else if (request.data.contains("/arsiv")) {
+            val yil = Calendar.getInstance().get(Calendar.YEAR)
             val sayfa = "?page=sayi&tab=1&sort=date_desc&filterType=2&imdbMin=5&imdbMax=10&yearMin=1900&yearMax=$yil"
             val replace = sayfa.replace("sayi", page.toString())
-            val document = app.get("${request.data}${replace}").document
-            val yeniEklenenler = document.select("a.w-full").mapNotNull { it.yeniEklenenler() }
-            return newHomePageResponse(request.name, yeniEklenenler)
+            document = app.get("${request.data}${replace}").document
+            document.select("a.w-full").mapNotNull { it.yeniEklenenler() }
         } else {
-            val document = app.get(request.data).document
-            val sonBolumler = document.select("div.col-span-3 a").mapNotNull { it.sonBolumler() }
-            return newHomePageResponse(request.name, sonBolumler)
+            document.select("div.col-span-3 a").mapNotNull { it.sonBolumler() }
         }
+
+        return newHomePageResponse(request.name, home)
     }
 
     private fun Element.diziler(): SearchResponse {
-        val title = this.selectFirst("span.font-normal")?.text() ?: ""
-        val href = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: ""
+        val title = this.selectFirst("span.font-normal")?.text() ?: "return null"
+        val href = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: "return null"
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
 
-        return newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
-            this.posterUrl = posterUrl
-        }
-    }
-
-    private fun PageItem.sayfalar(): SearchResponse {
-        val title = this.originalTitle ?: ""
-        val href = fixUrlNull(this.slug) ?: ""
-        val posterUrl = fixUrlNull(this.posterUrl?.replace("images-macellan-online.cdn.ampproject.org/i/s/", ""))
         return newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
             this.posterUrl = posterUrl
         }
@@ -153,7 +125,7 @@ class Dizilla : MainAPI() {
             this.posterUrl = posterUrl
         }
     }
-    private suspend fun Element.sonBolumler(): SearchResponse? {
+    private suspend fun Element.sonBolumler(): SearchResponse {
         val name = this.selectFirst("h2")?.text() ?: ""
         val epName = this.selectFirst("div.opacity-80")!!.text().replace(". Sezon ", "x")
             .replace(". Bölüm", "")
@@ -162,12 +134,22 @@ class Dizilla : MainAPI() {
 
         val epDoc = fixUrlNull(this.attr("href"))?.let { app.get(it).document }
 
-        val href = fixUrlNull(epDoc?.select("nav a")?.last()?.attr("href")) ?: return null
+        val href = fixUrlNull(epDoc?.selectFirst("div.poster a")?.attr("href")) ?: "return null"
 
-        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
+        val posterUrl = fixUrlNull(epDoc?.selectFirst("div.poster img")?.attr("src"))
 
         return newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
             this.posterUrl = posterUrl
+        }
+    }
+
+    private fun SearchItem.toSearchResponse(): SearchResponse? {
+        return newTvSeriesSearchResponse(
+            title ?: return null,
+            "${mainUrl}/${slug}",
+            TvType.TvSeries,
+        ) {
+            this.posterUrl = poster
         }
     }
 
@@ -176,6 +158,7 @@ class Dizilla : MainAPI() {
             "${mainUrl}/api/bg/searchcontent?searchterm=$query",
             headers = mapOf(
                 "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0",
+                "user-agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0",
                 "Accept" to "application/json, text/plain, */*",
                 "Accept-Language" to "en-US,en;q=0.5",
                 "X-Requested-With" to "XMLHttpRequest",
@@ -218,7 +201,7 @@ class Dizilla : MainAPI() {
     override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
 
     override suspend fun load(url: String): LoadResponse? {
-        val mainReq = app.get(url)
+        val mainReq = app.get(url, interceptor = interceptor)
         val document = mainReq.document
         val title = document.selectFirst("div.poster.poster h2")?.text() ?: return null
         val poster = fixUrlNull(document.selectFirst("div.w-full.page-top.relative img")?.attr("src"))
@@ -272,17 +255,41 @@ class Dizilla : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        val document = app.get(data).document
+        val document = app.get(data, interceptor = interceptor).document
         val script = document.selectFirst("script#__NEXT_DATA__")?.data()
         val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         val secureData = objectMapper.readTree(script).get("props").get("pageProps").get("secureData")
-        val decodedData = Base64.decode(secureData.toString().replace("\"", ""), Base64.DEFAULT).toString(Charsets.UTF_8)
+        val decodedData = decryptDizillaResponse(secureData.toString().replace("\"", ""))
         val source = objectMapper.readTree(decodedData).get("RelatedResults")
             .get("getEpisodeSources").get("result").get(0).get("source_content").toString()
             .replace("\"", "").replace("\\", "")
         val iframe = fixUrlNull(Jsoup.parse(source).select("iframe").attr("src")) ?: return false
         loadExtractor(iframe, "${mainUrl}/", subtitleCallback, callback)
         return true
+    }
+
+    private val privateAESKey = "9bYMCNQiWsXIYFWYAu7EkdsSbmGBTyUI"
+
+    private fun decryptDizillaResponse(response: String): String? {
+        try {
+            val algorithm = "AES/CBC/PKCS5Padding"
+            val keySpec = SecretKeySpec(privateAESKey.toByteArray(), "AES")
+
+            val iv = ByteArray(16)
+            val ivSpec = IvParameterSpec(iv)
+
+            val cipher1 = Cipher.getInstance(algorithm)
+            cipher1.init(Cipher.DECRYPT_MODE, keySpec,ivSpec)
+            val firstIterationData =
+                cipher1.doFinal(Base64.decode(response, Base64.DEFAULT))
+
+            val jsonString = String(firstIterationData)
+
+            return jsonString
+        } catch (e: Exception) {
+            Log.e("Dizilla", "Decryption failed: ${e.message}")
+            return null
+        }
     }
 }
