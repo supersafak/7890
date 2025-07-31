611c58d28d622a8baf68e1309f11964dadc24c82
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-07-31 16:53:16 +0300

    Update DiziYou.kt

--- DiziYou/src/main/kotlin/com/keyiflerolsun/DiziYou.kt
+++ DiziYou/src/main/kotlin/com/keyiflerolsun/DiziYou.kt
@@ -29,7 +29,7 @@ import com.lagradost.cloudstream3.utils.newExtractorLink
 import org.jsoup.nodes.Element
 
 class DiziYou : MainAPI() {
-    override var mainUrl = "https://www.diziyou11.com"
+    override var mainUrl = "https://www.diziyou12.com"
     override var name = "DiziYou"
     override val hasMainPage = true
     override var lang = "tr"
