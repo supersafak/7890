9f9dbc73f6a2bcdb1c193ff300edddc1459a8eef
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-09-04 17:28:08 +0300

    Update DiziMag.kt

--- DiziMag/src/main/kotlin/com/nikyokki/DiziMag.kt
+++ DiziMag/src/main/kotlin/com/nikyokki/DiziMag.kt
@@ -37,7 +37,7 @@ import org.jsoup.nodes.Element
 import java.util.regex.Pattern
 
 class DiziMag : MainAPI() {
-    override var mainUrl = "https://dizimag.me"
+    override var mainUrl = "https://dizimag.live"
     override var name = "DiziMag"
     override val hasMainPage = true
     override var lang = "tr"
