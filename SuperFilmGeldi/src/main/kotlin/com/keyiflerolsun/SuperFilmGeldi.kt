0094d7df1145b663fdf513db2b7874b3230de9d0
nikyokki <6808128+nikyokki@users.noreply.github.com>
2025-09-06 18:33:51 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- SuperFilmGeldi/src/main/kotlin/com/keyiflerolsun/SuperFilmGeldi.kt
+++ SuperFilmGeldi/src/main/kotlin/com/keyiflerolsun/SuperFilmGeldi.kt
@@ -29,7 +29,7 @@ import com.lagradost.cloudstream3.utils.newExtractorLink
 import org.jsoup.nodes.Element
 
 class SuperFilmGeldi : MainAPI() {
-    override var mainUrl = "https://www.superfilmgeldi8.art"
+    override var mainUrl = "https://www.superfilmgeldi9.art"
     override var name = "SuperFilmGeldi"
     override val hasMainPage = true
     override var lang = "tr"
